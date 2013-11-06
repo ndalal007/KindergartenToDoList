01754d2388fc79d96f40578f50baea872bb7859e
hujanais <teikmenglee@yahoo.com>
2013-11-05 23:36:32 -0500

    added SQLLite database support.

--- KindergartenToDoList/src/main/java/com/mobilesig/kindergartentodolist/MainActivity.java
+++ KindergartenToDoList/src/main/java/com/mobilesig/kindergartentodolist/MainActivity.java
@@ -3,6 +3,9 @@ package com.mobilesig.kindergartentodolist;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.Calendar;
+import java.util.List;
+
 public class MainActivity extends Activity {
 
     /* FIELDS */
@@ -12,6 +15,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vm = new ViewModel(getApplicationContext());
+
+        TestMethod();
     }
 
     @Override
@@ -25,5 +32,28 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+    private void TestMethod()
+    {
+        try
+        {
+            /* Test Add */
+            WorkItem newWorkItem = new WorkItem("description1", Calendar.getInstance().getTime(), WorkItem.PriorityEnum.NORMAL, WorkItem.StatusEnum.PENDING);
+            vm.AddWorkItem(newWorkItem);
+
+            // Test GetAll
+            List<WorkItem> allWorkItems = vm.GetAllWorkItems ();
+            int count = allWorkItems.size();
+
+            // Test Edit
+
+            // Test Delete
+            vm.DeleteWorkItem(allWorkItems.get(0).Id);
+
+            count = allWorkItems.size();
+        }
+        catch (Exception ex)
+        {}
     }
 }
